a640dcfe1276909f8329b1ad8b4907100f5f3f4d
Bertrand Polus <bertrand.polus@globalway.co.jp>
2016-05-26 10:46:49 +0900

    #1 #2

---
+++ src/project_euler/Problem_1_to_25/Project_Euler_Problem_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_euler.Problem_1_to_25;
+
+/**
+ *
+ * @author bpolus
+ */
+public class Project_Euler_Problem_1 {
+    
+    public static long findSum() {
+        long lSum = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) lSum += i;
+        }
+        return lSum;
+    }
+
+}
---
+++ src/project_euler/Project_Euler.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project_euler;
+
+
+/**
+ *
+ * @author bpolus
+ */
+public class Project_Euler {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Problem 2
+         System.out.println(project_euler.Problem_1_to_25.
+                                Project_Euler_Problem_2.findSum());
+
+        // Problem 1
+        // System.out.println(project_euler.Problem_1_to_25.
+        //                        Project_Euler_Problem_1.findSum());
+    }
+    
+}
